d562db0f6b362dbf1dc12e0f7343ce772e0c88b9
Navdeep Singh Rathore <navdeepsinghrathore2000@gmail.com>
2023-09-01 04:00:59 +0530

    chore: autogenerated templatee from ns3

---
+++ helper/wigig-module-helper.h
@@ -0,0 +1,14 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+#ifndef WIGIG_MODULE_HELPER_H
+#define WIGIG_MODULE_HELPER_H
+
+#include "ns3/wigig-module.h"
+
+namespace ns3 {
+
+/* ... */
+
+}
+
+#endif /* WIGIG_MODULE_HELPER_H */
+
---
+++ model/wigig-module.h
@@ -0,0 +1,12 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+#ifndef WIGIG_MODULE_H
+#define WIGIG_MODULE_H
+
+namespace ns3 {
+
+/* ... */
+
+}
+
+#endif /* WIGIG_MODULE_H */
+
